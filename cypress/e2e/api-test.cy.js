e768faafefe95439a73b9299b481f8b041de9a9c
humaidatun <142158353+humaidatun@users.noreply.github.com>
2025-03-27 15:07:10 +0700

    testing "tidak berhasil post pendaftaran ketika invalid token dan return 401 (unauthorized)"

--- cypress/e2e/api-test.cy.js
+++ cypress/e2e/api-test.cy.js
@@ -18,10 +18,10 @@ describe('API Testing - POST Pendaftaran', () => {
         kode_poli: 'UMUM',
         tanggalperiksa: '2022-07-12'
       },
-    }).then((Response) => {
-      expect(Response.status).to.eq(200);
-      expect(Response.body).to.have.property('status','success');
-      expect(Response.body).to.have.property('message','Pendaftaran berhasil');
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(responseesponse.body).to.have.property('status','success');
+      expect(response.body).to.have.property('message','Pendaftaran berhasil');
     });
   });
   it('Tidak berhasil post pendaftaran dengan tidak mengisi nama', () => {
@@ -35,11 +35,11 @@ describe('API Testing - POST Pendaftaran', () => {
         asuransi: '0000',
         no_asuransi: '',
         kode_poli: 'UMUM',
-        tanggalperiksa: '2022-07-12'
+        tanggalperiksa: '2022-07-12',
       }
-    }
+    })
   })
-  it('Tidak berhasil post pendaftaran dengan tidak mengisi nik' () => {
+  it('Tidak berhasil post pendaftaran dengan tidak mengisi nik', () => {
     cy.request({
       method: 'POST',
       url: apiUrl,
@@ -54,7 +54,7 @@ describe('API Testing - POST Pendaftaran', () => {
       }
     })
   })
-  it('Tidak berhasil post pendaftaran dengan tidak mengisi asuransi' () => {
+  it('Tidak berhasil post pendaftaran dengan tidak mengisi asuransi', () => {
     cy.request({
       method: 'POST',
       url: apiUrl,
@@ -69,7 +69,7 @@ describe('API Testing - POST Pendaftaran', () => {
       }
     })
   })
-  it('Tidak berhasil post pendaftaran dengan tidak mengisi kode poli' () => {
+  it('Tidak berhasil post pendaftaran dengan tidak mengisi kode poli', () => {
     cy.request({
       method: 'POST',
       url: apiUrl,
@@ -84,7 +84,7 @@ describe('API Testing - POST Pendaftaran', () => {
       }
     })
   })
-  it('Tidak berhasil post pendaftaran dengan tidak mengisi tanggalperiksa' () => {
+  it('Tidak berhasil post pendaftaran dengan tidak mengisi tanggalperiksa', () => {
     cy.request({
       method: 'POST',
       url: apiUrl,
@@ -95,15 +95,16 @@ describe('API Testing - POST Pendaftaran', () => {
         asuransi: '0000',
         no_asuransi: '',
         kode_poli: 'UMUM',
-        tanggalperiksa: '2022-07-12',
+        tanggalperiksa: '',
       }
     })
-  })
-  it('Tidak berhasil post pendaftaran dengan tidak mengisi seluruh data' () => {
+})
+  it('Tidak berhasil post pendaftaran dengan tidak mengisi seluruh data dan return 400', () => {
     cy.request({
       method: 'POST',
       url: apiUrl,
       headers: headers,
+      failOnStatusCode: false, 
       body: {
         nama: '',
         nik: '',
@@ -112,6 +113,36 @@ describe('API Testing - POST Pendaftaran', () => {
         kode_poli: '',
         tanggalperiksa: '',
       }
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      // expect(response.body).to.have.property('status','error');
+      expect(response.body.header).to.have.property('message','seluruh data harus diisi!');
     })
   })
+  it('Tidak berhasil post pendaftaran ketika invalid token dan return 401 (unauthorized)', () => {
+    cy.request({
+      method: 'POST',
+      url: apiUrl,
+      headers: {
+        'x-username': 'xxxxxxxxxx',
+        'x-token': 'invalid token',
+        'Content-Type': 'application/json',
+      },
+      failOnStatusCode: false,
+      body: {
+        nama: 'APRIS-SUNDIANA',
+        nik: '3211111111111111',
+        asuransi: '0000',
+        no_asuransi: '',
+        kode_poli: 'UMUM',
+        tanggalperiksa: '2022-07-12',
+      },
+    }).then((response) => {
+      cy.log(JSON.stringify(response, null, 2));
+
+      expect(response.status).to.eq(401);
+      // expect(response.body).to.have.property('status','error');
+      expect(response.body.header).to.have.property('message', 'Token tidak valid');
+    });
+  });
 })
